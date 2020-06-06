52a409d018836f4c6be0451e521a3345810f2410
Ralph Sleigh <ralph.sleigh@gmail.com>
2020-06-06 23:58:49 +0100

    Update index.js
    Remove add tag

--- index.js
+++ index.js
@@ -64,9 +64,7 @@ Toolkit.run(async tools => {
 
     const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`
     // console.log(Buffer.from(remoteRepo).toString('base64'))
-    await tools.runInWorkspace('git', ['tag', newVersion])
-    await tools.runInWorkspace('git', ['push', remoteRepo, '--follow-tags'])
-    await tools.runInWorkspace('git', ['push', remoteRepo, '--tags'])
+    await tools.runInWorkspace('git', ['push', remoteRepo])
   } catch (e) {
     tools.log.fatal(e)
     tools.exit.failure('Failed to bump version')
